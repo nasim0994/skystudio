d3c6d4f7322eb3196aeec097a4be656c5c42312f
mernshihab <shihabmd103@gmail.com>
2024-10-03 19:37:13 +0600

    fix2

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
 const { createJsonWebToken } = require("../utils/jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.addAdmin = async (req, res) => {
   try {
